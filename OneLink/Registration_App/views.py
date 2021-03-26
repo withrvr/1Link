5289faf830b3cb0ef337a51cd1a216728233a57c
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-03-27 01:11:56 +0530

    messages framework to make easy to understand site

--- OneLink/Registration_App/views.py
+++ OneLink/Registration_App/views.py
@@ -19,13 +19,33 @@ class UsersProfile_CreateView(SuccessMessageMixin, CreateView):
     template_name = 'Registration_App/Users_Register_Template.html'
     success_url = reverse_lazy('Registration_App:Login-Page')
     form_class = UsersProfile_CreationForm
-    success_message = "Your Account was <strong>Created Successfully!!</strong> ... To Continue <strong>Login</strong> into Your Account"
+    success_message = "Your Account was <strong>Created Successfully!!</strong>"
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        if request.method == "GET":
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Filling up this Information, <b>And Create the brand new Account</b>'
+            )
+        return response
 
 
 # login user
 class UsersProfile_LoginView(SuccessMessageMixin, LoginView):
     template_name = 'Registration_App/Users_Login_Template.html'
     success_message = 'You have <strong>Login</strong> succesfully !!!'
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        if request.method == "GET":
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                '<strong>Login</strong> to your Account'
+            )
+        return response
 
 
 # logout user
@@ -36,6 +56,6 @@ class UsersProfile_LogoutView(LogoutView):
         messages.add_message(
             request,
             messages.ERROR,
-            'You have been Logged out !!! <strong>Log in Again</strong>',
+            'You have been <strong>Logged out !!!</strong>',
         )
         return redirect('Registration_App:Login-Page')
