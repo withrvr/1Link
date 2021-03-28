6ab0feef25d92597fd07dd50ba3c866a3f77d35d
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-03-28 20:10:04 +0530

    Slice Detail ( view Slice info )

--- OneLink/Slices_App/urls.py
+++ OneLink/Slices_App/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import Slices_CreateView, Slices_ListView
+from .views import (
+    Slices_CreateView,
+    Slices_ListView,
+    Slices_DetailView_of_Login_User,
+)
 
 app_name = 'Slices_App'
 
 urlpatterns = [
     path('', Slices_ListView.as_view(), name='Slices-List-Page'),
     path('new/', Slices_CreateView.as_view(), name='Slices-Create-Page'),
+    path(
+        '<str:SliceName_From_URL_Of_Login_User>/',
+        Slices_DetailView_of_Login_User.as_view(),
+        name='Slice-Detail-Page-Of-Login-User'
+    ),
 ]
