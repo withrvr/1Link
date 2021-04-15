ec69d5c88136f25c1868971e4063d3cbad05549b
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-04-15 20:19:00 +0530

    API: show url as key and its hyperlink

--- OneLink/API_App/serializers.py
+++ OneLink/API_App/serializers.py
@@ -3,14 +3,20 @@ from rest_framework import serializers
 from UsersProfile_App.models import UsersProfile_Model
 from Slices_App.models import Slices_Model
 from Links_App.models import Links_Model
+from rest_framework import serializers
 
 
 # UsersProfile
 class UsersProfile_Serializers(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API_App:API-UsingId-Users-Retrieve-Page"
+    )
+
     class Meta:
         model = UsersProfile_Model
         fields = [
             'id',
+            'url',
 
             'username',
             'email',
@@ -26,10 +32,15 @@ class UsersProfile_Serializers(serializers.ModelSerializer):
 
 # Slices
 class Slices_Serializers(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API_App:API-Slices-Retrieve-Page"
+    )
+
     class Meta:
         model = Slices_Model
         fields = [
             'id',
+            'url',
             'author',
             'slice_Name',
             'visibility',
@@ -39,10 +50,15 @@ class Slices_Serializers(serializers.ModelSerializer):
 
 # Links
 class Links_Serializers(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API_App:API-Links-Retrieve-Page"
+    )
+
     class Meta:
         model = Links_Model
         fields = [
             'id',
+            'url',
             'display_Name',
             'link_url',
             'visibility',
