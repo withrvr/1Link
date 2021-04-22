8f81cda91f3e57fb36e7fec631e3b30bfb87f72a
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-04-22 19:39:54 +0530

    RANDOM: slices

--- OneLink/Core_App/urls.py
+++ OneLink/Core_App/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from .views import Home_View, Random_Users_View
+from .views import Home_View, Random_Users_View, Random_Slices_View
 app_name = 'Core_App'
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', Home_View.as_view(), name='Home-Page', ),
     path('home/', RedirectView.as_view(pattern_name='Core_App:Home-Page')),
 
-    # Random Users
+    # Random Users, Slices
     path('random-users/', Random_Users_View.as_view(), name='Random-Users-Page'),
+    path('random-slices/', Random_Slices_View.as_view(), name='Random-Slices-Page'),
 ]
