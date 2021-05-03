5a3a0227ad244f9c0b3e31790c18d9ecddbced42
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-05-03 20:49:08 +0530

    POPULAR: users and slices

--- src/Core_App/urls.py
+++ src/Core_App/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from .views import Home_View, Random_Users_View, Random_Slices_View
+from .views import (
+    Home_View,
+
+    # random
+    Random_Users_View,
+    Random_Slices_View,
+
+    # Popular
+    Popular_Users_View,
+    Popular_Slices_View,
+)
+
 app_name = 'Core_App'
 
 urlpatterns = [
@@ -9,7 +20,12 @@ urlpatterns = [
     path('', Home_View.as_view(), name='Home-Page', ),
     path('home/', RedirectView.as_view(pattern_name='Core_App:Home-Page')),
 
-    # Random Users, Slices
+    # Random Users & Slices
     path('random-users/', Random_Users_View.as_view(), name='Random-Users-Page'),
     path('random-slices/', Random_Slices_View.as_view(), name='Random-Slices-Page'),
+
+    # Popular Users & Slices
+    path('popular-users/', Popular_Users_View.as_view(), name='Popular-Users-Page'),
+    path('popular-slices/', Popular_Slices_View.as_view(),
+         name='Popular-Slices-Page'),
 ]
