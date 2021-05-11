2be2efed2c9dcb549bbb3eed50679a294f866c5e
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-05-11 13:57:11 +0530

    cant_use_this_name in ( slices_Name & usersname )

--- src/Slices_App/forms.py
+++ src/Slices_App/forms.py
@@ -2,6 +2,18 @@ from .models import Slices_Model
 from django import forms
 
 
+# checking if this slice name is allowed to be used or not
+def is_slice_name_allowed(self, *args, **kwargs):
+    validate_slice_name = self.cleaned_data["slice_Name"]
+
+    import cant_use_this_name
+    if validate_slice_name in cant_use_this_name.cant_use_this_name:
+        raise forms.ValidationError(
+            f"Can't use ''{validate_slice_name}'' as Slice Name - Try Something else"
+        )
+    return validate_slice_name
+
+
 # slice list form
 class Slices_ListForm(forms.ModelForm):
     class Meta:
@@ -11,6 +23,10 @@ class Slices_ListForm(forms.ModelForm):
 
 # slice creation form
 class Slices_CreationForm(forms.ModelForm):
+
+    def clean_slice_Name(self, *args, **kwargs):
+        return is_slice_name_allowed(self, *args, **kwargs)
+
     class Meta:
         model = Slices_Model
         fields = ['slice_Name', 'visibility', ]
@@ -18,6 +34,10 @@ class Slices_CreationForm(forms.ModelForm):
 
 # slice update form
 class Slices_UpdateForm(forms.ModelForm):
+
+    def clean_slice_Name(self, *args, **kwargs):
+        return is_slice_name_allowed(self, *args, **kwargs)
+
     class Meta:
         model = Slices_Model
         fields = ['slice_Name', 'visibility', ]
---
+++ src/cant_use_this_name.py
@@ -0,0 +1,35 @@
+cant_use_this_name = [
+    'login',
+    'logout',
+    'register',
+    'password',
+
+    'popular',
+    'random',
+
+    'home',
+    'about',
+    'api',
+
+    'static',
+    'new',
+
+    'lorem',
+    'ipsum',
+    'loremipsum',
+
+    'update',
+    'delete',
+    'create',
+
+    'users',
+    'slices',
+    'links',
+    'user',
+    'slice',
+    'link',
+
+    'sex',
+    'porn',
+    'fuck',
+]
