d579ee83b40900f55dfeed85eba67ffeb1fbe2e9
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-05-12 13:52:51 +0530

    static file managment

--- src/Core_App/static/Core_App/js/main.js
+++
@@ -1,8 +0,0 @@
-let user_profile_picture = document.getElementById("user_profile_picture");
-if (user_profile_picture) {
-	user_profile_picture.style.height = `${user_profile_picture.offsetWidth}px`;
-
-	window.addEventListener("resize", function (e) {
-		user_profile_picture.style.height = `${user_profile_picture.offsetWidth}px`;
-	});
-}
